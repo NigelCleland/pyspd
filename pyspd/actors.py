35f1fe0f77c87a6d6fa1fb01e76a78b64958434f
Nigel Cleland <nigel.cleland@gmail.com>
2013-09-09 12:35:45 +1200

    Merging iso into actors to use decorator functionality

--- pyspd/actors.py
+++ pyspd/actors.py
@@ -1,2 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+class SystemOperator(object):
+    """System Operator class takes all of the """
+    def __init__(self):
+        super(SystemOperator, self).__init__()
+
+        self.stations = []
+        self.nodes = []
+        self.reserve_zones = []
+        self.interruptible_loads = []
+
+    def add_station(self, Station):
+        """ Add a Station """
+
+        self.stations.append(Station)
+
+operator = SystemOperator()
+
+def addStation(cls):
+    def wrapped(cls):
+        print operator
+        operator.add_station(cls)
+        print cls
+
+    return wrapped
+
+class Company(object):
+    """docstring for Company"""
+    def __init__(self, arg):
+        super(Company, self).__init__()
+
+class Node(object):
+    """docstring for Node"""
+    def __init__(self, arg):
+        super(Node, self).__init__()
+        self.arg = arg
+
+class ReserveZone(object):
+    """docstring for ReserveZone"""
+    def __init__(self, arg):
+        super(ReserveZone, self).__init__()
+        self.arg = arg
+
+class Station(object):
+    """docstring for Station"""
+
+    @addStation
+    def __init__(self):
+        super(Station, self).__init__()
+
+
+class InterruptibleLoad(object):
+    """docstring for InterruptibleLoad"""
+    def __init__(self, arg):
+        super(InterruptibleLoad, self).__init__()
+        self.arg = arg
+
+
+if __name__ == '__main__':
+    pass
+
