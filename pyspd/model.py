aba5c945c4e10268b90f7f17cdb39fd35596f593
Nigel Cleland <nigel.cleland@gmail.com>
2013-09-09 16:01:10 +1200

    Create Variables for Linear Program

--- pyspd/model.py
+++ pyspd/model.py
@@ -35,10 +35,50 @@ class SPDModel(object):
 
         # Iterate over the difference instances:
 
-        for itname in self.ISO.itinstances:
+        pass
 
-            # Apply the Constraints for each instance
-            pass
+    def _create_variables(self):
+        """ Create all of the variables necessary to solve the Linear Program
+        This maps the variables from the ISO to the requisite Linear Program
+        Variables
+
+        Returns:
+        --------
+        energy_band_offers:
+        reserve_band_offers
+        transmission_band_offers
+        energy_total_offers
+        reserve_total_offers
+        transmission_total_offers
+        nodal_injection
+        reserve_zone_risk
+        """
+
+
+        self.energy_band_offers = self.lpDict("Energy_Band",
+                        self.ISO.energy_band_offers, 0)
+
+        self.reserve_band_offers = self.lpDict("Reserve_Band",
+                        self.ISO.reserve_band_offers, 0)
+
+        self.transmission_band_offers = self.lpDict("Transmission_Band",
+                        self.ISO.transmission_band_offers)
+
+        self.energy_total_offers = self.lpDict("Energy_Total",
+                        self.ISO.energy_total_offers, 0)
+
+        self.reserve_total_offers = self.lpDict("Reserve_Total",
+                        self.ISO.reserve_total_offers, 0)
+
+        self.transmission_total_offers = self.lpDict("Transmission_Total",
+                        self.ISO.transmission_total_offers)
+
+        self.nodal_injection = self.lpDict("Nodal_Injection",
+                        self.ISO.nodal_injection)
+
+        self.reserve_zone_risk = self.lpDict("Reserve_Risk",
+                        self.ISO.reserve_zone_risk, 0)
+
 
     def _nodal_demand(self):
         """ Apply the nodal demand constraints """
