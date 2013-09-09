27e82eadc6edaeae38b7d2ac76ec1ad987dc5d26
Nigel Cleland <nigel.cleland@gmail.com>
2013-09-09 13:03:18 +1200

    Adding initial structure to classes

--- pyspd/actors.py
+++ pyspd/actors.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# ----------------------------------------------------------------------------
+# SYSTEM OPERATOR
+# ----------------------------------------------------------------------------
+
 class SystemOperator(object):
     """System Operator class takes all of the """
     def __init__(self):
@@ -25,9 +29,14 @@ class SystemOperator(object):
         self.interruptible_loads.append(IL)
 
 # Define an Operator Class each time this is run!
+# This is a little bit of magic to make sure the decorators work
+# Automatically creates the operator class for the user
+# Do not change the ordering here!
 operator = SystemOperator()
 
-
+# ----------------------------------------------------------------------------
+# DECORATORS
+# ----------------------------------------------------------------------------
 
 def addStation(cls):
     def wrapped(cls, *args, **kargs):
@@ -50,40 +59,67 @@ def addIL(cls):
     return wrapped
 
 
+# ----------------------------------------------------------------------------
+# PARTICIPANT CLASSES
+# ----------------------------------------------------------------------------
+
+
 class Company(object):
     """docstring for Company"""
-    def __init__(self, arg):
+    def __init__(self, name):
         super(Company, self).__init__()
+        self.name = name
 
 
 class Node(object):
     """docstring for Node"""
     @addNode
-    def __init__(self, arg):
+    def __init__(self, name, RZ, demand=0):
         super(Node, self).__init__()
-        self.arg = arg
+        self.name = name
+        self.demand = demand
+
+        self.stations = []
+        self.intload = []
+
+        RZ._add_node(self)
+
+    def _add_station(self, Station):
+        self.stations.append(Station)
+
+    def _add_intload(self, IL):
+        self.intload.append(IL)
+
 
 class ReserveZone(object):
     """docstring for ReserveZone"""
     @addRZ
-    def __init__(self, arg):
+    def __init__(self, name):
         super(ReserveZone, self).__init__()
-        self.arg = arg
+        self.name = name
+        self.nodes = []
+
+    def _add_node(self, Node):
+        self.nodes.append(Node)
+
 
 class Station(object):
     """docstring for Station"""
 
     @addStation
-    def __init__(self):
+    def __init__(self, name, Node):
         super(Station, self).__init__()
+        self.name = name
+
+        Node._add_station(self)
 
 
 class InterruptibleLoad(object):
     """docstring for InterruptibleLoad"""
     @addIL
-    def __init__(self, arg):
+    def __init__(self, name):
         super(InterruptibleLoad, self).__init__()
-        self.arg = arg
+        self.name = name
 
 
 if __name__ == '__main__':
