6f62930d4357853ca08bf90016f6ff0b68379420
Nigel Cleland <nigel.cleland@gmail.com>
2013-09-09 12:42:45 +1200

    Working decorators for adding classes to the System Operator automatically

--- pyspd/__init__.py
+++ pyspd/__init__.py
@@ -4,3 +4,5 @@
 __author__ = 'Nigel Cleland'
 __email__ = 'nigel.cleland@gmail.com'
 __version__ = '0.1.0'
+
+from actors import *
--- pyspd/actors.py
+++ pyspd/actors.py
@@ -13,32 +13,59 @@ class SystemOperator(object):
 
     def add_station(self, Station):
         """ Add a Station """
-
         self.stations.append(Station)
 
+    def add_node(self, Node):
+        self.nodes.append(Node)
+
+    def add_reserve_zone(self, RZ):
+        self.reserve_zones.append(RZ)
+
+    def add_interruptible_load(self, IL):
+        self.interruptible_loads.append(IL)
+
+# Define an Operator Class each time this is run!
 operator = SystemOperator()
 
-def addStation(cls):
-    def wrapped(cls):
-        print operator
-        operator.add_station(cls)
-        print cls
 
+
+def addStation(cls):
+    def wrapped(cls, *args, **kargs):
+        operator.add_station(cls)
     return wrapped
+
+def addNode(cls):
+    def wrapped(cls, *args, **kargs):
+        operator.add_node(cls)
+    return wrapped
+
+def addRZ(cls):
+    def wrapped(cls, *args, **kargs):
+        operator.add_reserve_zone(cls)
+    return wrapped
+
+def addIL(cls):
+    def wrapped(cls, *args, **kargs):
+        operator.add_interruptible_load(cls)
+    return wrapped
+
 
 class Company(object):
     """docstring for Company"""
     def __init__(self, arg):
         super(Company, self).__init__()
 
+
 class Node(object):
     """docstring for Node"""
+    @addNode
     def __init__(self, arg):
         super(Node, self).__init__()
         self.arg = arg
 
 class ReserveZone(object):
     """docstring for ReserveZone"""
+    @addRZ
     def __init__(self, arg):
         super(ReserveZone, self).__init__()
         self.arg = arg
@@ -53,6 +80,7 @@ class Station(object):
 
 class InterruptibleLoad(object):
     """docstring for InterruptibleLoad"""
+    @addIL
     def __init__(self, arg):
         super(InterruptibleLoad, self).__init__()
         self.arg = arg
